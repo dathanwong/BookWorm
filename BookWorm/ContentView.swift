1e631e6efd7e73abf8395748cee589e1ef0d2439
dathwo@outlook.com <dathwo@outlook.com>
2020-06-18 08:39:07 -0700

    Finished first day of assignment

---
+++ BookWorm/ContentView.swift
@@ -0,0 +1,37 @@
+//
+//  ContentView.swift
+//  BookWorm
+//
+//  Created by Dathan Wong on 6/17/20.
+//  Copyright © 2020 Dathan Wong. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @Environment(\.managedObjectContext) var moc
+    @FetchRequest(entity: Book.entity(), sortDescriptors: []) var books: FetchedResults<Book>
+    @State private var showingAddScreen = false
+    
+    var body: some View {
+        NavigationView {
+            Text("Count: \(books.count)")
+                .navigationBarTitle("Bookworm")
+                .navigationBarItems(trailing: Button(action: {
+                    self.showingAddScreen.toggle()
+                }) {
+                    Image(systemName: "plus")
+                })
+                .sheet(isPresented: $showingAddScreen) {
+                    AddBookView().environment(\.managedObjectContext, self.moc)
+                }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
